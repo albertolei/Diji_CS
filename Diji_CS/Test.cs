ee02f9a49df8e0e351c24bca9c70780dbd05cddc
unknown <3132@nwepdi.net>
2018-06-01 16:46:19 +0800

    纵筋类型3,4,5-20180601

--- Diji_CS/Test.cs
+++ Diji_CS/Test.cs
@@ -155,8 +155,10 @@ namespace Diji_CS
             //app.ActiveModelReference.AddElement(spiral_stirrup);
             //app.ActiveModelReference.AddElement(bottom_stirrup);
             //Element stirrup = StirrupUtil.create_spiral_stirrup(1200.0, 200.0, 200.0, 100.0, 400.0, 100.0, 200.0);
-            Element stirrup = StirrupUtil.create_octagon_stirrup(1500, 1500, 500, 400);
-            //app.ActiveModelReference.AddElement(stirrup);
+            Element stirrup = StirrupUtil.create_column_stirrups(800, 800, 1500, 400, "5");
+            Element longitudinal = LongitudinalBarUtil.create_column_longitudinal_rebars(800, 800, 1500, 1500, 1500, 400, 50, "5");
+            app.ActiveModelReference.AddElement(stirrup);
+            app.ActiveModelReference.AddElement(longitudinal);
         }
 
         //public static void sayHello(string unparsed)
